bda1e145cc26ff88d3ce020eb493b9cce337f742
Tom Wartmann <tomw@ethz.ch>
2025-02-13 15:28:15 +0100

    add a review game, sth. the user can rate each job from a scale from 0,9

--- README.md
+++ README.md
@@ -3,6 +3,15 @@ This is a script that scrapes [jobs.ch](https://www.jobs.ch/en/) and [itjobs.ch]
 
 **Last Data Downloaded:** 13 February, 2025
 
+
+## Data Summary
+Visualizations of published IT related jobs from jobs.ch and itjobs.ch 
+### Publication Date
+![Plot of Publication Date Distribution](plots/time.png)
+* A line plot showing the daily count of publications since 1.1.2025.
+
+For more analysis, see `src/analysis.ipynb`
+
 ## **Setup Instructions**
 ### 1. Create and Activate the Conda Environment
 Create a Conda environment called `itjobs` from the `environment.yml` file and install the required dependencies.
@@ -13,19 +22,23 @@ conda activate itjobs
 ```
 ### 2. Run the Scraper
 If you want to download the latest data from jobs.ch and/or itjobs.ch run the scraper in `src/scraper.py`.
+
 Alternatively, you can skip this step and use the already downloaded data in `data/jobs.json`
 ```bash
 python src/scraper.py
 ```
-### 3. Proprocess the Data
+### 3. Review jobs
+You can review the downloaded jobs by running `src/review.py`. This each downloaded job (that is not rated so far) and the user can rate the job from 0-9.
+```bash
+python src/review.py
+```
+### 4. Preprocess the Data
 To clean and extract important information from the raw file jobs.json, run the `src/preprocessing.py` script, which will create the `jobs_processed.json` file in the data folder.
+
+Alternatively, you can skip this step and use the already preprocessed data in `data/jobs_preprocessed.json`
 ```bash
 python src/preprocessing.py
 ```
-## Data Summary
-Visualizations of published IT related jobs from jobs.ch and itjobs.ch 
-### Publication Date
-![Plot of Publication Date Distribution](plots/time.png)
-* A line plot showing the daily count of publications since 1.1.2025.
+
 
 
---
+++ src/review.py
@@ -0,0 +1,69 @@
+import json
+import os
+import random
+class JobReviewer:
+    def __init__(self, jobs_file='data/jobs.json'):
+        self.jobs = []
+        self.load_jobs(jobs_file)
+
+    def load_jobs(self, jobs_file):
+        try:
+            with open(jobs_file, 'r') as file:
+                self.jobs = json.load(file)
+        except FileNotFoundError:
+            print(f"File {jobs_file} not found.")
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON from file {jobs_file}.")
+    def save_jobs(self, jobs_file='data/jobs.json'):
+        try:
+            with open(jobs_file, 'w') as file:
+                json.dump(self.jobs, file, indent=4)
+        except Exception as e:
+            print(f"An error occurred while saving jobs to {jobs_file}: {e}")
+            
+    def show_job(self, job):
+        print("\n" + "-" * 50)
+        attributes = ['job_title', 'company', 'publication_date', 'place_of_work',  'job_category', 'contract_type', 'programming_languages','years', 'workload', 'salary', 'descriptions']
+        for attr in attributes:
+            if attr in job:
+                if attr == 'descriptions' and len(job[attr]) > 0:
+                    print("Descriptions:")
+                    for description in job[attr]:
+                        for key, value in description.items():
+                            print(f"  Section {key}:")
+                            for item in value:
+                                print(f"    - {item}")
+                else:
+                    print(f"{attr.replace('_', ' ').title()}: {job[attr]}")
+                    
+    def review_job(self, job) -> bool:
+        while True:
+            print("Please rate the job between 0 and 9 (or enter '.' to exit): ", end='', flush=True)
+            rating = input()
+            if rating == '.':
+                return False
+            if rating.isdigit() and 0 <= int(rating) <= 9:
+                job['rating'] = int(rating)
+                job['reviewed'] = True
+                
+                return True
+            else:
+                print("Invalid input. Please enter a number between 0 and 9 or '.' to exit.")
+    def review_random_jobs(self):
+        # iterate over jobs with clean job_title
+        unreviewed_jobs = [job for job in self.jobs if ('reviewed' not in job or not job['reviewed']) ]
+        print("Jobs to review: {}".format(len(unreviewed_jobs)))
+        for unreviewed_job in unreviewed_jobs:
+            self.show_job(unreviewed_job)
+            do_continue =self.review_job(unreviewed_job)
+            self.save_jobs()            
+            if not do_continue:
+                break
+        else:
+            print("No unreviewed jobs available.")
+
+if __name__ == "__main__":
+    parent_dir = os.path.dirname(os.path.dirname(__file__))
+    job_rater = JobReviewer()
+    job_rater.review_random_jobs()
+    
